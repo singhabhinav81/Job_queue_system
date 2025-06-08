0de49cca6fab232b368a7c26ffd97086e3044f8a
Abhinav Singh <abhinavsingh@Abhinavs-MacBook-Air.local>
2025-06-08 12:12:55 +0530

    UDPATED:: Database URL

--- db_init/create_db.sql
+++
@@ -1,8 +0,0 @@
--- init_jobs.sql
--- This script drops and recreates the 'jobs_db' database, then creates a 'jobs' table with test data.
-
--- Drop the database if it exists (must be run from a different database like 'postgres')
-DROP DATABASE IF EXISTS jobs_db;
-
--- Create the new database
-CREATE DATABASE jobs_db;
